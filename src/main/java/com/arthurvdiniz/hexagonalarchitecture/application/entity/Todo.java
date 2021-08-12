b22b17c8e509a7cee533e3c79cbf9c1788ba6ba9
Arthur Diniz <arthurvdinizs@gmail.com>
2021-08-12 14:42:10 -0300

    fix(db): h2 impl

--- src/main/java/com/arthurvdiniz/hexagonalarchitecture/application/entity/Todo.java
+++ src/main/java/com/arthurvdiniz/hexagonalarchitecture/application/entity/Todo.java
@@ -5,11 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Todo {
+@Entity
+public class Todo implements Serializable {
 
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
   private Integer id;
   private String title;
   private String description;
